cd56811f0d2c2954ed1620621a63c79bea75eb8f
vika-anikina <tiltoslave@gmail.com>
2024-10-23 19:33:41 +0300

    first commit

---
+++ AssignmentVika5-1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AssignmentVika5_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Guess a number between 1 and 100:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(269, 139);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 5;
+            this.txtGuess.TextChanged += new System.EventHandler(this.txtGuess_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(269, 165);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(266, 200);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Result";
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Location = new System.Drawing.Point(381, 200);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(48, 13);
+            this.lblAttempts.TabIndex = 8;
+            this.lblAttempts.Text = "Attempts";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Guess a number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblAttempts;
+    }
+}
+
---
+++ AssignmentVika5-1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentVika5_1
+{
+    public partial class Form1 : Form
+    {
+        private int randomNumber;
+        private int numberOfGuesses;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+        private void InitializeGame()
+        {
+            Random rand = new Random();
+            randomNumber = rand.Next(1, 101);
+            numberOfGuesses = 0;
+            lblResult.Text = "";
+            lblAttempts.Text = "";
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtGuess_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int userGuess;
+            if (int.TryParse(txtGuess.Text, out userGuess))
+            {
+                numberOfGuesses++;
+                if (userGuess > randomNumber)
+                {
+                    lblResult.Text = "Too high, try again.";
+                }
+                else if (userGuess < randomNumber)
+                {
+                    lblResult.Text = "Too low, try again.";
+                }
+                else
+                {
+                    lblResult.Text = "Congratulations! You guessed the number!";
+                    lblAttempts.Text = $"It took you {numberOfGuesses} attempts.";
+                    MessageBox.Show($"Congratulations! You guessed the number! It took you {numberOfGuesses} attempts. Try again!");
+                    InitializeGame();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+    }
+}
